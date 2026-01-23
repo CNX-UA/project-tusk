d74a0cce0b72163e11508a02e9d02fee20b60a3b
CNX-UA <cnx.ukraine@gmail.com>
2026-01-23 03:11:04 +0200

    final touches for Auth funtionallity to make it work with react frontend by overriding devise Sessions and Registrations controller, updated routed.db with clean paths, addition of blueprinter  gem for flat JSON structure

---
+++ apps/api/app/controllers/registrations_controller.rb
@@ -0,0 +1,20 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json:{
+        status: {code: 200, message: "Singed up successfully."},
+        data: UserBlueprint.render_as_hash(resource)
+      }
+    else
+      render json: {
+        status: { 
+          message: "Registration failed. #{resource.errors.full_messages.to_sentence}"
+        }
+      }, status: :unprocessable_entity
+    end
+  end
+end
--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users, defaults: { format: :json },
+  devise_for :users,
+              path: "",
+              path_names: {
+              sign_in: "login",
+              sign_out: "logout",
+              registration: "signup"
+              },
               controllers: {
+                sessions: "users/sessions",
+                registrations: "users/registrations",
                 omniauth_callbacks: "users/omniauth_callbacks"
-              }
+              },
+              defaults: { format: :json }
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index]
