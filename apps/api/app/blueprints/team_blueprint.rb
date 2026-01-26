9f145be6612f92ef12de43ebde1033fa15a1349a
CNX-UA <cnx.ukraine@gmail.com>
2026-01-26 20:52:34 +0200

    deleted leftover code

--- apps/api/app/blueprints/team_blueprint.rb
+++ apps/api/app/blueprints/team_blueprint.rb
@@ -1,8 +1,4 @@
 class TeamBlueprint < Blueprinter::Base
   identifier :id
   fields :name, :department_type, :users_count, :created_at
-
-  field :members_count do |team|
-    team.users.count
-  end
 end
