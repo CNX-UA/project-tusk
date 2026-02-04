6d8f5aae26f8f22cf44270e278b12c8a7a043575
Oleg Zavedia <olegzavedia0@gmail.com>
2026-02-04 18:20:11 +0200

    realized projects create function, created modal for create project

--- apps/web/src/features/projects/api/projectsRequests.js
+++ apps/web/src/features/projects/api/projectsRequests.js
@@ -1,12 +1,12 @@
 import api from "@/api/axios"
 
 export const fetchProjects = async () => {
-    const response =  await api.get("/projects");
+    const response =  await api.get("/api/v1//projects");
 
     return response.data.data || response.data;
 };
 
 export const createProject = async (projectData) => {
-    const response = await api.post("/projects", {project: projectData});
+    const response = await api.post("/api/v1//projects", {project: projectData});
     return response.data.data;
 };
