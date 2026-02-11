dc5261ad5ade9deef0d3ffa4e787b95632ffc91f
CNX-UA <cnx.ukraine@gmail.com>
2026-02-11 18:23:29 +0200

    Update production.rb
    updated with config.hosts to allow, otherwise will block any requests due to security features

--- apps/api/config/environments/production.rb
+++ apps/api/config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
+  config.hosts << ENV['RAILS_HOST'] if ENV['RAILS_HOST'].present? 
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
