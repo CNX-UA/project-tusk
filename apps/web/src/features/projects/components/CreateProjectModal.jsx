7cd4daac5d0cc793588341cfa0076cbf278bc57a
Oleg Zavedia <olegzavedia0@gmail.com>
2026-02-12 10:55:33 +0200

    updated project creating modal

--- apps/web/src/features/projects/components/CreateProjectModal.jsx
+++ apps/web/src/features/projects/components/CreateProjectModal.jsx
@@ -1,130 +1,252 @@
-import React, {useEffect} from 'react';
-import { useFormik } from 'formik';
-import { 
-  Dialog, DialogTitle, DialogContent, DialogActions, 
-  TextField, Button, Box, Typography, InputAdornment 
-} from '@mui/material';
-import TagIcon from '@mui/icons-material/Tag';
-import { projectSchema } from '../schema/projectSchema';
-import { useCreateProject } from '../hooks/useCreateProject';
-import { useToast } from '@/context/ToastProvider';
+import React, { use, useEffect } from "react";
+import { useFormik } from "formik";
+import {
+  TextField,
+  Button,
+  Box,
+  Typography,
+  InputAdornment,
+  Grid, 
+} from "@mui/material";
 
-const CreateProjectModal = ({ open, onClose }) => {
-    const { showToast } = useToast();
+import TagIcon from "@mui/icons-material/Tag";
+import FolderIcon from "@mui/icons-material/Folder";
 
-    const createMutation = useCreateProject(() => {
-        showToast('Project created successfully', 'success');
-        handleClose();
-    });
+import { projectSchema } from "../schema/projectSchema";
+import { useCreateProject } from "../hooks/useCreateProject";
+import { useToast } from "@/context/ToastProvider";
+import { useProjectMutation } from "../hooks/useProjectMutation";
+import  ProjectTypeCard from "./ProjectTypeCard";
 
-    const formik = useFormik({
-        initialValues: {
-            title: '',
-            key: '',
-            description: ''
-        },
-        validationSchema: projectSchema,
-        onSubmit: (values) => {
-            createMutation.mutate(values);
-        },
-    });
+import EntityModal from "@/components/ui/EntityModal";
 
-    useEffect(() => {
-        if (!formik.touched.key && formik.values.title) {
-            const generatedKey = formik.values.title
-                .split(' ')
-                .map(word => word[0])
-                .join('')
-                .toUpperCase()
-                .slice (0,4);
+const CreateProjectModal = ({ open, onClose, projectData }) => {
+  const isEdit = !!projectData;
+  const { showToast } = useToast();
+  const { updateMutation } = useProjectMutation();
+  const createMutation = useCreateProject(() => {
+    showToast("Project created successfully", "success");
+    handleClose();
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      key: "",
+      description: "",
+      // project_type: "board",
+    },
+    enableReinitialize: isEdit,
+    validationSchema: projectSchema,
+    onSubmit: (values) => {
+      if (isEdit) {
+        updateMutation.mutate(
+          { id: projectData.id, projectData: values },
+          {
+            onSuccess: () => {
+              showToast("Project updated successfully", "success");
+              handleClose();
+            },
+            onError: (error) => {
+              showToast("Failed to update project", "error");
+              console.error(error);
+            },
+          },
+        );
+      } else createMutation.mutate(values);
+    },
+  });
+
+  useEffect(() => {
+    if (isEdit && projectData) {
+      formik.setValues({
+        title: projectData.title || "",
+        key: projectData.key || "",
+        description: projectData.description || "",
+      });
+    }
+  }, [isEdit, projectData]);
+
+  useEffect(() => {
+    if (!isEdit && formik.values.title) {
+      const cleanTitle = formik.values.title
+        .trim()
+        .toUpperCase()
+        .replace(/[^A-Z0-9\s]/g, "");
+      const words = cleanTitle.split(/\s+/).filter(Boolean);
+
+      let generatedKey = "";
+
+      if (words.length >= 3) {
+        generatedKey = words
+          .map((w) => w[0])
+          .join("")
+          .slice(0, 5);
+      } else {
+        generatedKey = cleanTitle.replace(/\s/g, "").slice(0, 3);
+      }
+
+      formik.setFieldValue("key", generatedKey);
+    }
+  }, [formik.values.title, isEdit]);
+
+  const handleClose = () => {
+    formik.resetForm();
+    createMutation.reset();
+    onClose();
+  };
+
+  const modalAction = (
+    <>
+      <Button onClick={handleClose} color="inherit">
+        Cancel
+      </Button>
+      <Button
+        type="submit"
+        variant="contained"
+        disabled={createMutation.isPending || !formik.isValid}
+        onClick={formik.handleSubmit}
+      >
+        {createMutation.isPending
+          ? "Creating..."
+          : isEdit
+            ? "Save Changes"
+            : "Create Project"}
+      </Button>
+    </>
+  );
+
+  return (
+    <EntityModal
+      open={open}
+      onClose={handleClose}
+      title={isEdit ? "Edit Project" : "Create New Project"}
+      actions={modalAction}
+    >
+      <Box>
+        <Typography
+          variant="caption"
+          color="text.secondary"
+          sx={{ mb: 1.5, display: "block", fontWeight: "bold" }}
+        >
+          PROJECT TYPE
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <ProjectTypeCard
+              label="Task Board"
+              value="board"
+              // selected={formik.values.project_type === "board"}
+              onClick={(val) => formik.setFieldValue("project_type", val)}
+              disabled={true}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <ProjectTypeCard
+              label="Folder"
+              icon={FolderIcon}
+              value="folder"
+              // selected={formik.values.project_type === "folder"}
+              onClick={(val) => formik.setFieldValue("project_type", val)}
+              disabled={true}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box sx={{ display: "flex", gap: 2 }}>
+        <Typography
+          variant="caption"
+          fontWeight="bold"
+          color="text.secondary"
+          sx={{ mb: 1.5, display: "block" }}
+        >
+          DETAILS
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <TextField
+              autoFocus
+              fullWidth
+              id="title"
+              name="title"
+              label="Project Name"
+              placeholder="e.g. Website Redesign"
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
+              sx={{ flexGrow: 1 }}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              id="key"
+              name="key"
+              label="Key"
+              placeholder="WEB"
+              value={formik.values.key}
+              disabled={true}
+              onBlur={formik.handleBlur}
+              error={formik.touched.key && Boolean(formik.errors.key)}
+              helperText={formik.touched.key && formik.errors.key}
+              InputProps={{
+                readOnly: true,
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <TagIcon fontSize="small" />
+                  </InputAdornment>
+                ),
+              }}
+              sx={{
+                width: 140,
+                "& .MuiInputBase-input.Mui-disabled": {
+                  WebkitTextFillColor: "#1976d2", // Робимо текст ключа синім (колір бренду), щоб підсвітити його
+                  fontWeight: "bold",
+                },
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Box>
+
+        <TextField
+          fullWidth
+          multiline
+          rows={3}
+          id="description"
+          name="description"
+          label="Description (Optional)"
+          placeholder="What is this project about?"
+          value={formik.values.description}
+          onChange={formik.handleChange}
+        />
+    </EntityModal>
+    /* <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
+      <DialogTitle sx={{ fontWeight: "bold" }}>{isEdit ? 'Edit Project' : 'Create New Project'}</DialogTitle>
+
+        <DialogContent>
+          <Typography variant="body2" color="text.secondary" sx={{ mb: 3 }}>
+            {isEdit ? 'Update your workspace details' : 'Create a workspace to track everything'}
+          </Typography>
+
+          <Box sx={{ display: "flex", gap: 2, mb: 2 }}>
+
+
             
-            formik.setFieldValue('key', generatedKey)
-        }
-    }, [formik.values.title]);
+          </Box>
 
-    const handleClose = () => {
-        formik.resetForm();
-        createMutation.reset();
-        onClose();
-    };
+          
 
-    return (
-        <Dialog open={open} onClose={handleClose} fullWidth maxWidth="sm">
-            <DialogTitle sx={{ fontWeight: 'bold' }}>Create New Project</DialogTitle>
-
-            <form onSubmit={formik.handleSubmit}>
-                <DialogContent>
-                    <Typography variant='body2' color='text.secondary' sx={{ mb: 3 }}>
-                        Create a workspace to track everything
-                    </Typography>
-
-                    <Box sx={{ display: 'flex', gap: 2, mb: 2 }}>
-                        <TextField 
-                        autoFocus
-                        fullWidth
-                        id="title"
-                        name="title"
-                        label="Project Name"
-                        placeholder="e.g. Website Redesign"
-                        value={formik.values.title}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        error={formik.touched.title && Boolean(formik.errors.title)}
-                        helperText={formik.touched.title && formik.errors.title}
-                        sx={{ flexGrow: 1 }}
-                        />
-
-                        <TextField 
-                        sx={{ width: 140 }}
-                        id="key"
-                        name="key"
-                        label="Key"
-                        placeholder="WEB"
-                        value={formik.values.key}
-                        onChange={(e) => {
-                            formik.setFieldValue('key', e.target.value.toUpperCase());
-                        }}
-                        onBlur={formik.handleBlur}
-                        error={formik.touched.key && Boolean(formik.errors.key)}
-                        helperText={formik.touched.key && formik.errors.key}
-                        InputProps={{
-                            startAdornment: (
-                            <InputAdornment position="start">
-                                <TagIcon fontSize="small" />
-                            </InputAdornment>
-                            ),
-                        }} 
-                        />
-                    </Box>
-
-                    <TextField 
-                    fullWidth
-                    multiline
-                    rows={3}
-                    id="description"
-                    name="description"
-                    label="Description (Optional)"
-                    placeholder="What is this project about?"
-                    value={formik.values.description}
-                    onChange={formik.handleChange}
-                    />
-
-                   {createMutation.isError && (         
-                    <Typography color='error' variant='body2' sx={{ mt: 2 }}>
-                        {createMutation.error?.response?.data?.status?.message || console.log(createMutation.error)}
-                    </Typography>
-                   )}
-                </DialogContent>
-
-                <DialogActions sx={{ px: 3, pb: 2 }}>
-                    <Button onClick={handleClose} color='inherit'>Cancel</Button>
-                    <Button type='submit' variant='contained' disabled={createMutation.isPending || !formik.isValid}
-                    >
-                        {createMutation.isPending ? 'Creating...' : 'Create Project'}
-                    </Button>
-                </DialogActions>
-            </form>
-        </Dialog>
-    );
+          {createMutation.isError && (
+            <Typography color="error" variant="body2" sx={{ mt: 2 }}>
+              {createMutation.error?.response?.data?.status?.message ||
+                console.log(createMutation.error)}
+            </Typography>
+          )}
+        </DialogContent>
+    </Dialog> */
+  );
 };
 export default CreateProjectModal;
